eceeedc63e4790a7572dde7e37843d308364a19a
Inna Ianko <inna.i@mate.academy>
2025-05-26 12:50:34 +0300

    Minor style fixe

--- tests/signUpFormElements.spec.js
+++ tests/signUpFormElements.spec.js
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('`Sign up` form elements presence', async ({ page }) => {
-/*
+  /*
   1. Go to the Conduit Sign-up page https://conduit.mate.academy/user/register
   2. Assert that the header 'Sign up' is visible
   3. Assert that the link 'Have an account?' is visible
@@ -9,6 +9,5 @@ test('`Sign up` form elements presence', async ({ page }) => {
   5. Assert that the input field 'Email' is visible
   6. Assert that the input field 'Password' is visible
   7. Assert that the button 'Sign up' is visible
- */
-
+  */
 });
